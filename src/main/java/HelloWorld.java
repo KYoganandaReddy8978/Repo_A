ba1d791bf4136ae51372fa121a16e2998cb84de8
yogananda-fs <yogananda.kanchisamudram@firstgroup.com>
2025-03-03 12:11:44 +0530

    Initial Spring Boot project commit for Repo_A to copy to B 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void helloWorlds()
     {
-        System.out.println("Hello World from class Hello World new commit");
+        System.out.println("Hello World from class Hello World new commit inton Repo B");
     }
 }
