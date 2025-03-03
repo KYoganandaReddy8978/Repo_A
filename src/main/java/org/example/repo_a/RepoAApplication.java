3922f918785079f6781c89669a64e9fd12b5ec36
yogananda-fs <yogananda.kanchisamudram@firstgroup.com>
2025-03-03 11:17:30 +0530

    Initial Spring Boot project commit for Repo_A

---
+++ src/main/java/org/example/repo_a/RepoAApplication.java
@@ -0,0 +1,14 @@
+package org.example.repo_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoAApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is Repo _ A");
+        SpringApplication.run(RepoAApplication.class, args);
+    }
+
+}
